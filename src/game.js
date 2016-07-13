be398adb988af3e5893987365a66dd492dbd8470
Victor Duan <vduan@fb.com>
2016-07-13 16:45:04 -0700

    collision v1

--- src/game.js
+++ src/game.js
@@ -26,8 +26,11 @@ state.create = function () {
 
   this.player = new Kiwi.GameObjects.Sprite( this, this.textures.player, 350, 550 );
   this.addChild( this.player );
+  this.playerrect = new Kiwi.Geom.Rectangle( this.player.x, this.player.y, this.player.width, this.player.height );
+
   this.missile = new Kiwi.GameObjects.Sprite( this, this.textures.missile, 500, 400);
   this.addChild( this.missile );
+  this.missilerect = new Kiwi.Geom.Rectangle( this.missile.x, this.missile.y, this.missile.width, this.missile.height );
 
 }
 
@@ -35,12 +38,26 @@ state.create = function () {
 state.update = function () {
   Kiwi.State.prototype.update.call( this );
   this.missile.x -= 5;
+  this.missilerect.x -= 5;
   if( this.missile.x < -this.missile.width ) {
 		this.missile.x = 800;
+    this.missilerect.x = 800;
     this.missile.y = getRandomInt(0, 800);
+    this.missilerect.y = this.missile.y;
 	}
 
+  this.checkCollisions();
+
 }
+
+state.checkCollisions = function () {
+  if (Kiwi.Geom.Intersect.rectangleToRectangle(this.playerrect, this.missilerect).result) {
+    console.log('yay');
+    FBInstant.game.setScore(5);
+    FBInstant.game.asyncYieldControl();
+  }
+}
+
 
 var gameOptions = {
   width: 800,
